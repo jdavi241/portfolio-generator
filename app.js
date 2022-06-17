1b14c6d9a48c271f90f8eb413d1b53e07bff7d3d
jdavi241 <davisjimm25@gmail.com>
2022-06-16 19:44:58 -0500

    err

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hello node!");
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
